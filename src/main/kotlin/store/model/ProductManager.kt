5a67f9ffcb7316acc22031935e2594a9b150afb2
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-08 02:31:48 +0900

    feat(ProductManager): products.md로부터 데이터를 모델에 저장하는 기능 추가

--- src/main/kotlin/store/model/ProductManager.kt
+++ src/main/kotlin/store/model/ProductManager.kt
@@ -1,37 +1,77 @@
 package store.model
 
-import org.w3c.dom.stylesheets.LinkStyle
-import store.utils.ConvertUtil.toProduct
-import store.utils.ConvertUtil.toPromotion
+import store.utils.ExtensionUtil.toPromotion
 import java.io.File
 
 class ProductManager() {
 
-    private val stocks = mutableListOf<StockEntity>()
-
     init {
-        readStocks()
+        readProducts()
     }
 
-    private fun readStocks() {
-        val filepath = "products.md"
-        File(filepath).forEachLine {
-            val split = it.split(",")
-            stocks.add(
-                StockEntity(
-                    split[0],
-                    split[1].toInt(),
-                    split[2].toInt(),
-                    split[3]
-                )
-            )
+    private fun readProducts() {
+        File(PRODUCTS_FILE_PATH).useLines { lines ->
+            lines.drop(1).forEach { line ->
+                getProductLine(line)
+            }
         }
     }
 
-    private fun getStocks(): List<StockEntity> = stocks.toList()
+    private fun getProductLine(line: String) {
+        val (name, price, quantity, promotion) = line.split(",")
+        stocks.find { it.name == name }?.let {
+            addQuantity(it, quantity, promotion.toPromotion())
+        } ?: run {
+            addStock(name, price, quantity, promotion.toPromotion())
+        }
+    }
 
-    private fun getProducts(): List<Product> =
-        stocks.map { it.toProduct() }.toList()
+    private fun addQuantity(
+        stock: Stock,
+        quantity: String,
+        promotion: Promotion
+    ) {
+        if (promotion == Promotion.NULL) {
+            stock.quantity += quantity.toInt()
+            return
+        }
+        stock.promotionQuantity += quantity.toInt()
+    }
 
+    private fun addStock(name: String, price: String, quantity: String, promotion: Promotion) {
+        if (promotion == Promotion.NULL) {
+            addDefaultStock(name, price, quantity)
+            return
+        }
+        addPromotionStock(name, price, quantity, promotion)
+    }
 
+    private fun addDefaultStock(name: String, price: String, quantity: String) {
+        stocks.add(
+            Stock(
+                name, price.toInt(), quantity.toInt(), Promotion.NULL, promotionQuantity = 0
+            )
+        )
+    }
+
+    private fun addPromotionStock(
+        name: String,
+        price: String,
+        quantity: String,
+        promotion: Promotion
+    ) {
+        stocks.add(
+            Stock(
+                name, price.toInt(), quantity = 0, promotion, promotionQuantity = quantity.toInt()
+            )
+        )
+    }
+
+    fun getStocks(): List<Stock> = stocks.toList()
+
+    companion object {
+        private val stocks = mutableListOf<Stock>()
+        private const val PRODUCTS_FILE_PATH = "src/main/resources/products.md"
+        private const val NULL = "null"
+    }
 }
---
+++ src/test/kotlin/store/StockTest.kt
@@ -0,0 +1,102 @@
+package store
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import store.model.ProductManager
+import store.model.Promotion
+import store.model.Stock
+
+class StockTest {
+    @Test
+    fun `재고 정보를 읽어오는지 확인한다`() {
+        val productManager = ProductManager()
+
+        val stocks = productManager.getStocks()
+
+
+
+        assertEquals(stocks, fileStocks)
+    }
+
+    companion object {
+        val fileStocks = listOf(
+            Stock(
+                name = "콜라",
+                price = 1000,
+                quantity = 10,
+                promotion = Promotion.CARBONIC_ACID,
+                promotionQuantity = 10
+            ),
+            Stock(
+                name = "사이다",
+                price = 1000,
+                quantity = 7,
+                promotion = Promotion.CARBONIC_ACID,
+                promotionQuantity = 8
+            ),
+            Stock(
+                name = "오렌지주스",
+                price = 1800,
+                quantity = 0,
+                promotion = Promotion.MD_RECOMMEND,
+                promotionQuantity = 9
+            ),
+            Stock(
+                name = "탄산수",
+                price = 1200,
+                quantity = 0,
+                promotion = Promotion.CARBONIC_ACID,
+                promotionQuantity = 5
+            ),
+            Stock(
+                name = "물",
+                price = 500,
+                quantity = 10,
+                promotion = Promotion.NULL,
+                promotionQuantity = 0
+            ),
+            Stock(
+                name = "비타민워터",
+                price = 1500,
+                quantity = 6,
+                promotion = Promotion.NULL,
+                promotionQuantity = 0
+            ),
+            Stock(
+                name = "감자칩",
+                price = 1500,
+                quantity = 5,
+                promotion = Promotion.FLASH_SALE,
+                promotionQuantity = 5
+            ),
+            Stock(
+                name = "초코바",
+                price = 1200,
+                quantity = 5,
+                promotion = Promotion.MD_RECOMMEND,
+                promotionQuantity = 5
+            ),
+            Stock(
+                name = "에너지바",
+                price = 2000,
+                quantity = 5,
+                promotion = Promotion.NULL,
+                promotionQuantity = 0
+            ),
+            Stock(
+                name = "정식도시락",
+                price = 6400,
+                quantity = 8,
+                promotion = Promotion.NULL,
+                promotionQuantity = 0
+            ),
+            Stock(
+                name = "컵라면",
+                price = 1700,
+                quantity = 10,
+                promotion = Promotion.MD_RECOMMEND,
+                promotionQuantity = 1
+            )
+        )
+    }
+}
