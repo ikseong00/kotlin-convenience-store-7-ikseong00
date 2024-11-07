bae14aa3e721a6bd872b382878eea52a68850870
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-07 15:23:08 +0900

    feat(Promotion): promotion.md 의 정보를 저장하는 enum class 생성
    TODO : 파일에서 읽어오는 기능을 추가해야 함.

---
+++ src/main/kotlin/store/model/Promotion.kt
@@ -0,0 +1,34 @@
+package store.model
+
+import java.time.LocalDateTime
+
+enum class Promotion(
+    val promotionName: String?,
+    val buy: Int?,
+    val get: Int?,
+    val startDate: LocalDateTime?,
+    val endDate: LocalDateTime?
+) {
+
+    // TODO : 파일에서 읽어오는 기능을 추가해야 함
+    CARBONIC_ACID(
+        "탄산2+1", 2, 1,
+        LocalDateTime.of(2024, 1, 1, 0, 0),
+        LocalDateTime.of(2024, 12, 31, 0, 0)
+    ),
+    MD_RECOMMEND(
+        "MD추천상품", 1, 1,
+        LocalDateTime.of(2024, 1, 1, 0, 0),
+        LocalDateTime.of(2024, 12, 31, 0, 0)
+    ),
+    FLASH_SALE(
+        "반짝할인", 1, 1,
+        LocalDateTime.of(2024, 11, 1, 0, 0),
+        LocalDateTime.of(2024, 11, 30, 0, 0)
+    ),
+    NULL(
+        null, null, null, null, null
+    )
+
+
+}
