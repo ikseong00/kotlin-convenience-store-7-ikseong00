7652f01c5427306928a39004b1aa4686755de6d9
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-08 01:08:38 +0900

    feat(InputView): 프로모션 재고 부족 시 정가 여부를 입력받는 기능 추가

--- src/main/kotlin/store/utils/message/InputMessages.kt
+++ src/main/kotlin/store/utils/message/InputMessages.kt
@@ -3,5 +3,6 @@ package store.utils.message
 object InputMessages {
     const val PURCHASE_INPUT_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"
     const val NOW = "현재 "
-    const val PROMOTION_INTRODUCTION_INPUT_MESSAGE= "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"
+    const val PROMOTION_INTRODUCTION_INPUT_MESSAGE = "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"
+    const val PRICE_INTRODUCTION_INPUT_MESSAGE = "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"
 }
--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -2,6 +2,8 @@ package store.view
 
 import camp.nextstep.edu.missionutils.Console
 import store.utils.Validator
+import store.utils.message.InputMessages.NOW
+import store.utils.message.InputMessages.PRICE_INTRODUCTION_INPUT_MESSAGE
 import store.utils.message.InputMessages.PROMOTION_INTRODUCTION_INPUT_MESSAGE
 import store.utils.message.InputMessages.PURCHASE_INPUT_MESSAGE
 
@@ -12,10 +14,18 @@ object InputView {
         return Console.readLine()
     }
 
-    fun getPromotionAddition(): String {
-        println(PROMOTION_INTRODUCTION_INPUT_MESSAGE)
+    fun getPromotionAddition(inputProductName: String): String {
+        println(
+            NOW + inputProductName + PROMOTION_INTRODUCTION_INPUT_MESSAGE
+        )
         return Console.readLine()
     }
 
+    fun getNoneDiscountPromotion(inputProductName: String, inputProductQuantity: Int): String {
+        println(
+            "$NOW$inputProductName $inputProductQuantity$PRICE_INTRODUCTION_INPUT_MESSAGE"
+        )
+        return Console.readLine()
+    }
 
 }
