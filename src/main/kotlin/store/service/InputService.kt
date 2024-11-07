06f8e3102093a7af4664e1340ad0534374ba64c9
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-08 01:35:34 +0900

    feat(InputService): 각 구매품목과 수량을 모델 리스트로 변환하는 기능 추가

--- src/main/kotlin/store/service/InputService.kt
+++ src/main/kotlin/store/service/InputService.kt
@@ -1,10 +1,21 @@
 package store.service
 
-import store.model.Product
+import store.model.PurchaseProduct
+import store.model.Stock
 import store.utils.Validator
 import store.view.InputView
 
-class InputService {
+class InputService(
+    private val stocks: List<Stock>
+) {
+
+    private fun separatedInfoToProducts(separatedInfo: List<String>): List<PurchaseProduct> {
+        return separatedInfo.map {
+            val (name, quantity) = it.split("-")
+            Validator.validateProductPurchasable(name, quantity.toInt(), stocks)
+            PurchaseProduct(name, quantity.toInt())
+        }.toList()
+    }
 
     private fun separatePurchaseInput(): List<String> {
         val input = InputView.getPurchaseInfo()
