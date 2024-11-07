7d4ce8c09819de2111383246bfd3ddebad1871ae
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-08 01:03:58 +0900

    feat(InputView): 프로모션 상품 혜택 안내 후 추가 입력 기능 추가

--- src/main/kotlin/store/utils/message/InputMessages.kt
+++ src/main/kotlin/store/utils/message/InputMessages.kt
@@ -2,4 +2,6 @@ package store.utils.message
 
 object InputMessages {
     const val PURCHASE_INPUT_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"
+    const val NOW = "현재 "
+    const val PROMOTION_INTRODUCTION_INPUT_MESSAGE= "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"
 }
--- src/main/kotlin/store/view/InputView.kt
+++ src/main/kotlin/store/view/InputView.kt
@@ -2,14 +2,20 @@ package store.view
 
 import camp.nextstep.edu.missionutils.Console
 import store.utils.Validator
+import store.utils.message.InputMessages.PROMOTION_INTRODUCTION_INPUT_MESSAGE
 import store.utils.message.InputMessages.PURCHASE_INPUT_MESSAGE
 
 object InputView {
+
     fun getPurchaseInfo(): String {
         println(PURCHASE_INPUT_MESSAGE)
         return Console.readLine()
     }
 
+    fun getPromotionAddition(): String {
+        println(PROMOTION_INTRODUCTION_INPUT_MESSAGE)
+        return Console.readLine()
+    }
 
 
 }
