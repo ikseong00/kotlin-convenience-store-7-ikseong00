6798e17cebee8ef5c1a5958ca64b69837921f6c4
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2024-11-10 19:26:44 +0900

    feat(Controller): 주 로직 주석으로 기술 및 Readme 에 추가

---
+++ src/main/kotlin/store/controller/StoreController.kt
@@ -0,0 +1,35 @@
+package store.controller
+
+import store.service.ProductService
+import store.model.PurchaseProduct
+import store.model.Stock
+import store.service.InputService
+
+// 0. 파일로부터 정보를 읽어옴
+// 1. 구매 수량과 품목을 입력받음
+// 2. 프로모션 상품인지 확인
+// 2.1. 프로모션 상품이고, 프로모션 재고가 충분한 지 확인
+// 2.1.1. 프로모션 재고가 충분하면, 추가 혜택 여부를 물어봄
+// 2.1.2. 프로모션 재고가 부족하면, 정가 결제 여부를 물어봄
+// 2.2 일반 상품이면, 그냥 계산함
+
+// 3. 멤버십 여부를 물어봄
+// 4. 총 가격과 수량을 계산
+// 5. 영수증 출력
+// 6. 재고 상황 업데이트
+// 7. 추가 구매 여부를 물어봄.
+// 7.1. 추가 구매 시 1번부터 다시 시작
+// 7.2. 아니면 프로그램 종료
+class StoreController {
+
+    private val stocks = ProductService.getStocks()
+    private lateinit var purchaseProducts: List<PurchaseProduct>
+
+
+    fun run() {
+
+        purchaseProducts = InputService(stocks).getPurchaseInfoToProducts()
+
+    }
+
+}
